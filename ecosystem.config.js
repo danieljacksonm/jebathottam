73e6cbf66d7a2c4425fcf1c438cdfd7cfe14b13e
danieljacksonm <jacksonmanidanial@gmail.com>
2026-08-08 18:02:35 +0530

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -53,6 +53,24 @@ module.exports = {
       log_file: "/home/dani/logs/sri-krishna-mobiles.log",
       error_file: "/home/dani/logs/sri-krishna-mobiles-error.log",
       out_file: "/home/dani/logs/sri-krishna-mobiles-out.log"
+    },
+    {
+      name: "canaan-travel",
+      cwd: "/home/dani/agntix-template",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3003
+      },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
+      log_file: "/home/dani/logs/canaan-travel.log",
+      error_file: "/home/dani/logs/canaan-travel-error.log",
+      out_file: "/home/dani/logs/canaan-travel-out.log"
     }
   ]
 };
